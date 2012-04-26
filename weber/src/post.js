e058d7c3f91773972d24c4ea7fb5335c0049737d
Andre Taddeini <ataddeini@gmail.com>
2012-04-25 20:48:50 -0500

    Refactored away from 'Microsoft style', and begen moving out some js and creating some QUnit tests.

---
+++ weber/src/post.js
@@ -0,0 +1,12 @@
+﻿var post = function(raw) {
+
+    raw.publishedDateDisplay = function () {
+        var pubDate = new Date(raw.publishedDate);
+//        var month = pubDate.getMonth();
+//        var day = pubDate.getDay();
+//        var year = pubDate.getYear();
+//        return month + " " + day + ", " + year;
+    };
+
+    return raw;
+};
---
+++ weber/src/tests/post.tests.js
@@ -0,0 +1,16 @@
+﻿///<reference path="/assets/js/libs/qunit.js"/> 
+///<reference path="/src/post.js"/>
+
+//TODO: Would be nice for the jQuery dependency not to be here
+$(function() {
+    
+    test("NewPost_WithRawFeedData_FormatsPublishDisplayDate", function() {
+        var raw = { publishedDate: "Wed, 29 Oct 2011 14:14:48 +0000" };
+        
+        var testPost = new post(raw);
+        var result = testPost.publishedDateDisplay();
+        
+        equal(result, "October 29, 2011 9:14pm CDT");
+    });
+    
+});
