577ac5e59bd4b1e93a76b59d2e0bcccbf00ec866
Andre Taddeini <ataddeini@gmail.com>
2012-06-04 13:02:43 -0500

    Refactoring to Backbone.js

---
+++ source/utils.js
@@ -0,0 +1,33 @@
+﻿///<reference path="/assets/scripts/moment.min.js"/>
+
+var utils = function(moment, $) {
+    return {
+        dates: {
+            formatDate: function(args) {
+                var parsedDate = Date.parse(args.date);
+
+                if (isNaN(parsedDate)) {
+                    return "n/a";
+                } else {
+                    return moment(args.date).format(args.format);
+                }
+            }
+        },
+
+        ajax: {
+            getFeed: function(url, callback) {
+                //TODO: How to go about testing this? Should the google feed loader be part of this?
+                $.ajax({
+                    url: document.location.protocol +
+                        "//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=" +
+                        encodeURIComponent(url),
+                    dataType: "json",
+                    //TODO: Handle failure here
+                    success: function(data) {
+                        callback(data.responseData.feed);
+                    }
+                });
+            }
+        }
+    };
+};
